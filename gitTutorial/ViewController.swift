df953e51d14b7946952b03e19427c2a4da26187c
Jimmy <chuneleu@gmail.com>
2019-05-31 18:55:50 -0700

    implemented image detection

--- gitTutorial/ViewController.swift
+++ gitTutorial/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Networking code here")
+        
+        print ("Image detection")
     }
 
 
