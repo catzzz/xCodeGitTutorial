642ad3b1d445b587c67607ef3e9f7c829304bb6f
Jimmy <chuneleu@gmail.com>
2019-05-31 18:49:17 -0700

    Initial Commit

---
+++ gitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTutorial
+//
+//  Created by Jimmy Leu on 2019/5/31.
+//  Copyright © 2019 Jimmy Leu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
